4f162d76e625571a5fe4ee040c121ad271091a0f
Ali <ali.cast.santos@gmail.com>
2025-03-27 16:13:16 -0300

    feat(modules): create base module structure for auth, quotes, notes, alerts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { QuotesModule } from './quotes/quotes.module';
+import { NotesModule } from './notes/notes.module';
+import { AlertsController } from './alerts/alerts.controller';
+import { AlertsModule } from './alerts/alerts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, QuotesModule, NotesModule, AlertsModule],
+  controllers: [AppController, AlertsController],
   providers: [AppService],
 })
 export class AppModule {}
